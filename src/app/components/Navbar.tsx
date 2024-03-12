0047429fc79a07f1b5aaad7a5e9f88db84836b64
Vishesh <visheshdpanchal@gmail.com>
2024-03-12 20:11:21 +0530

    feat:made the dropdown menus and popovers

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,6 @@
-import React, { useContext } from "react";
+"use client";
+
+import React, { useContext, useState } from "react";
 import Image from "next/image";
 import {
   DropdownMenu,
@@ -7,37 +9,258 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+import { Slider } from "@/components/ui/slider";
+
 import { AuthContext } from "@/app/Context/AuthContext";
+
+
 const Navbar = () => {
+  const [value, setValue] = useState(4);
   const { user } = useContext(AuthContext);
-  let url;
+  let url, email, name;
   if (user) {
     url = user.picture;
+    email = user.email;
+    name = user.name;
   }
+  // console.log(isOpen);
   return (
     <div className="flex ">
       <div className="flex-1 text-2xl font-semibold text-neutral-800">
         Projects
       </div>
       <div className="flex justify-center items-center gap-8">
-        <button className="flex text-orange-600 font-semibold bg-orange-50 gap-2 px-4 py-2 border border-orange-400 rounded-md text-sm items-center mr-8">
-          <Image src="/credit.svg" alt="icon" width={20} height={20} />
-          <p>Your Credits</p>
-          <Image src="/add.svg" alt="icon" width={25} height={25} />
-        </button>
-        <button>
-          <Image src="/notification.svg" alt="icon" width={20} height={25} />
-        </button>
-        <button>
-          <Image
-            src={url!}
-            alt="icon"
-            width={40}
-            height={25}
-            className="rounded-full"
-          />
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="outline-none">
+            <div className="flex text-orange-600 font-semibold bg-orange-50 gap-2 px-4 py-2 border border-orange-400 rounded-md text-sm items-center mr-8">
+              <Image src="/credit.svg" alt="icon" width={20} height={20} />
+              <p>Your Credits:4</p>
+              <Image src="/add.svg" alt="icon" width={25} height={25} />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className=" w-[26em] ">
+            <DropdownMenuLabel className="flex flex-col gap-1 justify-center items-center mt-2">
+              <Image src="/buy.svg" alt="icon" width={20} height={20} />
+              <p className="text-base">Buy more credits</p>
+              <p className="text-sm text-neutral-400 font-normal">
+                1 video requires around 2 credits
+              </p>
+            </DropdownMenuLabel>
+            <DropdownMenuItem
+              className="flex-col flex "
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+            >
+              <div className="flex gap-2 w-full">
+                <Slider
+                  defaultValue={[value]}
+                  max={100}
+                  step={1}
+                  onValueChange={(i) => setValue(i[0])}
+                  onClick={(e) => e.stopPropagation()}
+                  className=" outline-none w-3/4"
+                />
+                <p>{value} credits</p>
+              </div>
+              <div>
+                <p className="text-sm text-neutral-400">1 credit ≈ $2</p>
+              </div>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex gap-4">
+              <button className="px-4 py-2 text-neutral-700 border border-neutral-300 rounded-md flex-1 flex justify-center">
+                Cancel
+              </button>
+              <button className="px-4 py-2 bg-[#3C50E0] text-white rounded-md flex-1 flex justify-center">
+                Buy
+              </button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger className="outline-none">
+            <div>
+              <Image
+                src="/notification.svg"
+                alt="icon"
+                width={20}
+                height={25}
+              />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-64">
+            <DropdownMenuLabel className="flex justify-between text-sm">
+              <p className="font-semibold">Notification</p>
+              <p className="text-neutral-400 cursor-pointer">Clear All</p>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator className="w-full border border-neutral-200 " />
+            <DropdownMenuItem className="flex flex-col items-start text-sm text-neutral-600">
+              <p className=" text-start">
+                Edit your information in a swipe Sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim.
+              </p>
+              <p className="text-xs text-neutral-400 mt-1">12 May, 2025</p>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-col items-start text-sm text-neutral-600">
+              <p className=" text-start">
+                It is a long established fact that a reader will be distracted
+                by the readable.
+              </p>
+              <p className="text-xs text-neutral-400 mt-1">24 Feb, 2025</p>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex flex-col items-start text-sm text-neutral-600">
+              <p className=" text-start">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered
+              </p>
+              <p className="text-xs text-neutral-400 mt-1">04 Jan, 2025</p>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="outline-none">
+            <div>
+              <Image
+                src={url!}
+                alt="icon"
+                width={40}
+                height={25}
+                className="rounded-full cursor-pointer"
+              />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-fit" onClick={(e)=>{e.preventDefault();
+          e.stopPropagation()}}>
+            <DropdownMenuLabel className="flex gap-2 ">
+              <Image
+                src={url!}
+                alt="icon"
+                width={40}
+                height={25}
+                className="rounded-full cursor-pointer"
+              />
+              <div>
+                <div className="flex justify-between">
+                  <p>{name}</p>
+                  <p className="rounded-sm text-xs px-1 py-0.5 text-neutral-400 bg-neutral-200 h-fit">
+                    Free
+                  </p>
+                </div>
+                <p className="font-normal text-neutral-600">{email}</p>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="flex gap-4 font-sm"
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+            >
+              <Image
+                src="/account.svg"
+                alt="icon"
+                width={20}
+                height={20}
+                className="rounded-full cursor-pointer"
+              />
+              <Popover >
+                <PopoverTrigger onClick={(e)=>{e.stopPropagation()}} >
+                  <p>Account</p>
+                </PopoverTrigger>
+                <PopoverContent className="flex flex-col w-96" onClick={(e)=>{e.stopPropagation()}}>
+                  <div className="flex flex-col">
+                    <p>Account</p>
+                    <p className="text-xs text-neutral-400">
+                      Choose the avatar that best describes your use case
+                    </p>
+                  </div>
+                  <div className="flex flex-col gap-2 mt-4">
+                    <div className="flex flex-col text-sm">
+                      <p className="text-sm text-neutral-500">Display Name</p>
+                      <p>{name}</p>
+                    </div>
+                    <div className="flex flex-col text-sm">
+                      <p className="text-sm text-neutral-500">Email Address</p>
+                      <p>{email}</p>
+                    </div>
+                    <div className="flex flex-col text-sm justify-center">
+                      <p className="text-sm text-neutral-500">Current Plan</p>
+                      <div className="flex justify-between items-center">
+                        <p>Free</p>
+                        <button className="flex px-4 py-2 bg-[FF4D00] rounded-sm justify-start bg-orange-50">
+                          <Image
+                            src="/upgradePlan.svg"
+                            alt="myproject"
+                            width={20}
+                            height={20}
+                          />
+                          <p className="flex-1">Upgrade Plan</p>
+                          <Image
+                            src="/arrow-right.svg"
+                            alt="myproject"
+                            width={20}
+                            height={20}
+                          />
+                        </button>
+                      </div>
+                      <div className="w-full rounded-md bg-slate-200 flex gap-2 text-slate-700 px-4 py-2 mt-2">
+                      <Image
+                            src="/info.svg"
+                            alt="myproject"
+                            width={20}
+                            height={20}
+                          />
+                        <p> Your Credits = 24</p>
+                      </div>
+                      <div className="w-full bg-orange-500 rounded-md text-white text-sm flex justify-between px-4 py-2 mt-4 items-center">
+                        <p>
+                          Want to join the affliate program?
+                        </p>
+                        <button className="rounded-md bg-transparent border-white border px-3 py-1">
+                            Join now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex gap-4 font-sm">
+              <Image
+                src="/support.svg"
+                alt="icon"
+                width={20}
+                height={20}
+                className="rounded-full cursor-pointer"
+              />
+              <p>Support</p>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex gap-4 text-sm">
+              <Image
+                src="/log-out.svg"
+                alt="icon"
+                width={20}
+                height={20}
+                className="rounded-full cursor-pointer"
+              />
+              <p>Log-out</p>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex gap-2 justify-center text-neutral-400 text-xs">
+              <p className="underline">Privacy Policy</p>
+              <p className="underline">Terms & condition</p>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 import Image from "next/image";
 import { data } from "../constants";
 const Page = () => {
-  const [selected,setSelected] = useState(1);
+  const [selected, setSelected] = useState(1);
   const { user, isAuthenticated } = useContext(AuthContext);
   return (
     <div className="flex w-screen h-screen">
@@ -57,63 +57,62 @@ const Page = () => {
             <div className="flex justify-between">
               <div className="flex border-b-2 border-neutral-700 py-2 gap-2">
                 <Image
-                src='/template.svg'
-                alt='template'
-                width={20}
-                height={20}
+                  src="/template.svg"
+                  alt="template"
+                  width={20}
+                  height={20}
                 />
                 <p>Templates</p>
               </div>
               <div className="flex text-neutral-400 ml-auto gap-2 text-sm items-center ">
-                  <button
-                  onClick={()=>setSelected(1)}
-                  className={`rounded-md px-4 py-2 border-neutral-300  border ${selected===1?" text-black border-neutral-800 ":""}`}>
-                      All
-                  </button>
-                  <button className={`rounded-md px-4 py-2 gap-2 flex border-neutral-300 border ${selected===2?" text-black border-neutral-800 ":""}`}
-                  onClick={()=>setSelected(2)}
-
-                  >
+                <button
+                  onClick={() => setSelected(1)}
+                  className={`rounded-md px-4 py-2 border-neutral-300  border ${selected === 1 ? " text-black border-neutral-800 " : ""}`}
+                >
+                  All
+                </button>
+                <button
+                  className={`rounded-md px-4 py-2 gap-2 flex border-neutral-300 border ${selected === 2 ? " text-black border-neutral-800 " : ""}`}
+                  onClick={() => setSelected(2)}
+                >
                   <Image
-                src='/landscape.svg'
-                alt='Landscape'
-                width={20}
-                height={20}
-                />
-                      <p>Landscape Video (16:9)</p>
-                  </button>
-                  <button className={`rounded-md px-4 py-2 gap-2 flex border-neutral-300 border ${selected===3?" text-black border-neutral-800 ":""}`}
-                  onClick={()=>setSelected(3)}
-
-                  >
+                    src="/landscape.svg"
+                    alt="Landscape"
+                    width={20}
+                    height={20}
+                  />
+                  <p>Landscape Video (16:9)</p>
+                </button>
+                <button
+                  className={`rounded-md px-4 py-2 gap-2 flex border-neutral-300 border ${selected === 3 ? " text-black border-neutral-800 " : ""}`}
+                  onClick={() => setSelected(3)}
+                >
                   <Image
-                src='/potrait.svg'
-                alt='potrait'
-                width={10}
-                height={10}
-                />
-                      <p>Potrait Video (9:16)</p>
-                  </button>
+                    src="/potrait.svg"
+                    alt="potrait"
+                    width={10}
+                    height={10}
+                  />
+                  <p>Potrait Video (9:16)</p>
+                </button>
               </div>
             </div>
 
-              <div className=" grid grid-cols-4 gap-4 mt-4 mb-12 gap-y-4">
-                {
-                  (data[selected]).map((template)=>{
-                    return(
-                      <div key={template.src} className="rounded-md flex flex-col ">
-                        <Image
-                          src={template.src}
-                          alt='template'
-                          width={350}
-                          height={400}
-                          />
-                          <p className="text-sm font-semibold">{template.text}</p>
-                      </div>
-                    );
-                  })
-                }
-              </div>
+            <div className=" grid grid-cols-4 gap-4 mt-4 mb-12 gap-y-4">
+              {data[selected].map((template) => {
+                return (
+                  <div key={template.src} className="rounded-md flex flex-col ">
+                    <Image
+                      src={template.src}
+                      alt="template"
+                      width={350}
+                      height={400}
+                    />
+                    <p className="text-sm font-semibold">{template.text}</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
